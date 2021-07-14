d0c75d8381cb118108026b2c8449187adcb832ec
Tani1015 <1015ryusuke1015@gmail.com>
2021-07-14 19:11:33 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ message }}
+    <button @click="this.test">test</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'HelloWorld',
+  data:()=>{
+    return{
+      message: 'Hello Vue!'
+    }
+  },
+  methods: {
+    test(){
+      axios
+        .get('http://test/school/')
+        .then( response =>{
+            this.message = response
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+  }
+}
+</script>
